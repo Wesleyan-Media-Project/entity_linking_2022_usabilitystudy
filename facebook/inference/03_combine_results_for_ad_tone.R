6ac234fe3679afb0633f8817a3046b8c57d2ecf8
Markus Neumann <mneumann.polsci@gmail.com>
2023-01-17 17:53:34 -0500

    entity linking results for ad tone, with disclaimer and page name excluded

---
+++ facebook/inference/03_combine_results_for_ad_tone.R
@@ -0,0 +1,47 @@
+# Post-processing for the entity linking results
+# Gather up all detected entities from different fields and put them all together
+
+# This is for ad tone, so we exclude disclaimer and page_name
+
+library(data.table)
+library(dplyr)
+library(tidyr)
+library(stringr)
+
+# Paths
+# In
+path_detected_entities <- "../data/entity_linking_results_fb22_notext.csv.gz"
+# Out
+path_finished_enties <- "../data/detected_entities_fb22_for_ad_tone.csv.gz"
+
+
+# Read in Spacy's detected entities
+el <- fread(path_detected_entities)
+
+# Transform the Python-based detected entities field so it can be split later
+el$text_detected_entities <- str_remove_all(el$text_detected_entities, "\\[|\\]|\\'")
+el$text_detected_entities <- str_remove_all(el$text_detected_entities, " ")
+# Remove all ads with no detected entities
+el <- el %>% filter(text_detected_entities != "")
+# Split the Python-based id field
+el$id <- str_split(el$id, "\\|")
+# # Remove the ad field, we only care about ad id here
+# el$id <- lapply(el$id, str_remove, "__(.*?)$")
+# Keep only ad id and detected entities
+el <- el %>% select(id, text_detected_entities)
+# Unnest, this makes each row belong to separate ads
+# "Re-hydrate" in WMP lingo
+el <- unnest(el, cols = id)
+# Split the id into id and field
+el <- separate(el, id, c("id", "field"), "__")
+# Omit disclaimer for ad_tone
+el <- el %>% filter(!field %in% c("disclaimer", "page_name"))
+# Combine the detected entities from different fields of each ad
+el <- aggregate(el$text_detected_entities, by = list(el$id), c)
+el$x <- lapply(el$x, paste, collapse = ",")
+el$x <- str_split(el$x, ",")
+names(el) <- c("ad_id", "detected_entities")
+
+# Save version with combined fields
+fwrite(el, path_finished_enties)
+
