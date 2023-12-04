1489e03fb97c9ed3f5eeb2e8447d8c29816ad595
furkmak <93638016+furkmak@users.noreply.github.com>
2023-12-04 17:02:56 -0500

    Update 03_combine_results.R

--- google/inference/03_combine_results.R
+++ google/inference/03_combine_results.R
@@ -1,14 +1,11 @@
 # Post-processing for the entity linking results
-# First, fix Dartanyon Williams by making him a House, not Senate candidate
-# Then, save that result as it is, with separate fields
-# Finally, gather up all detected entities from different fields and put them all together
 
 library(data.table)
 library(dplyr)
 library(tidyr)
 library(stringr)
 
-el <- fread("../../../data/entity_linking_results_google_2020.csv.gz")
+el <- fread("../../../data/entity_linking_results_google_2022.csv.gz")
 
 #----
 # Fix Dartanyon Williams by making him a House, not Senate candidate
@@ -27,5 +24,5 @@ el[el == ""] <- NA
 el <- el %>% 
   unite(col = detected_entities, ends_with('detected_entities'), sep = ", ", na.rm = T)
 # Save version with combined fields
-fwrite(el, "../../../data/entity_linking_results_google_2020_notext_combined.csv.gz")
+fwrite(el, "../../../data/entity_linking_results_google_2022_notext_combined.csv.gz")
 
