001de29a1c23d980dec34b943b866de13ad4b996
furkmak <93638016+furkmak@users.noreply.github.com>
2023-12-06 19:14:50 -0500

    Add files via upload

---
+++ google/inference/03_combine_results_v2.R
@@ -0,0 +1,52 @@
+# Post-processing for the entity linking results
+# First, fix Dartanyon Williams by making him a House, not Senate candidate
+# Then, save that result as it is, with separate fields
+# Finally, gather up all detected entities from different fields and put them all together
+library(tidyverse)
+library(data.table)
+library(dplyr)
+library(tidyr)
+library(stringr)
+
+combined <- fread("google/data/entity_linking_results_google_2022.csv.gz")
+#----
+# Combine fields
+combined2 <- combined %>%
+  select(ad_id, ends_with('detected_entities'), field) %>% 
+  mutate(across(ends_with('detected_entities'), function(x){str_remove_all(x, "\\[|\\]|\\'")}))
+
+combined2[combined2 == ""] <- NA
+
+combined3 <- combined2 %>% 
+  unite(col = detected_entities, ends_with('detected_entities'), sep = ", ", na.rm = T)
+
+# Remove all ads with no detected entities
+combined4 <- combined3 %>% filter(detected_entities != "")
+
+# For ad tone, remove disclaimer and page_name
+combined4_at <- combined4 %>% filter(!field %in% c("advertiser_name"))
+
+# Aggregate based on ad_id
+combined5 <- combined4 %>%
+  group_by(ad_id) %>%
+  summarize(
+    detected_entities = paste(unique(detected_entities), collapse = ", "),
+    field = paste(unique(field), collapse = ", ")
+  )
+
+
+combined5_at <- combined4_at %>%
+  group_by(ad_id) %>%
+  summarize(
+    detected_entities = paste(unique(detected_entities), collapse = ", "),
+    field = paste(unique(field), collapse = ", ")
+  )
+
+
+# Save version with combined fields
+fwrite(combined5, "google/data/entity_linking_results_google_2022_notext_combined.csv.gz")
+fwrite(combined5_at, "google/data/entity_linking_results_google_2022_notext_combined_for_ad_tone.csv.gz")
+
+
+
+
