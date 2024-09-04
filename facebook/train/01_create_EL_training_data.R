5f0bee8d86003ed67293e54e3a0e037c4b2368d9
bella.tassone <itassone@wesleyan.edu>
2024-09-04 13:13:19 -0400

    updating train/01

--- facebook/train/01_create_EL_training_data.R
+++ facebook/train/01_create_EL_training_data.R
@@ -18,19 +18,19 @@ library(tidyr)
 #        'ad_creative_link_description', 'aws_ocr_text_img',
 #        'google_asr_text', 'aws_ocr_text_vid'
 #############################################################################################
-#GET FIGSHARE LINK
-#Make sure you place the file in the same directory as entity_linking_2022
+# GET FIGSHARE LINK
+# Make sure you place the file in the same directory as entity_linking_2022
 path_ads <- "../fb_2022_adid_text.csv.gz"
 
-#This is the output table from `data-post-production/01-merge-results/01_merge_preprocessed_results`
-#GET FIGSHARE LINK
-#Make sure you place the file in the same directory as entity_linking_2022
+# This is the output table from `data-post-production/01-merge-results/01_merge_preprocessed_results`
+# GET FIGSHARE LINK
+# Make sure you place the file in the same directory as entity_linking_2022
 path_adid_to_pageid <- "../fb_2022_adid_var1.csv.gz"
 
 path_entities_kb <- "facebook/data/entity_kb.csv"
 
-#This file is located in our datasets repository (https://github.com/Wesleyan-Media-Project/datasets)
-#Make sure the datasets folder is located in the same directory as entity_linking_2022
+# This file is located in our datasets repository (https://github.com/Wesleyan-Media-Project/datasets)
+# Make sure the datasets folder is located in the same directory as entity_linking_2022
 path_wmpent_file <- "../datasets/wmp_entity_files/Facebook/2022/wmp_fb_2022_entities_v120122.csv" # nolint: line_length_linter.
 # Output files
 path_output <- "facebook/data/ads_with_aliases.csv.gz"
@@ -38,17 +38,20 @@ path_output <- "facebook/data/ads_with_aliases.csv.gz"
 # Pdid to wmpid
 wmpents <- fread(path_wmpent_file) %>%
   select(pd_id, wmpid)
-wmpents <- wmpents[wmpents$wmpid != "",]
+wmpents <- wmpents[wmpents$wmpid != "", ]
 
 # Ads
 df <- fread(path_ads, encoding = "UTF-8")
 
-cols <- ['ad_id', 'page_name', 'disclaimer', 'ad_creative_body', 'ad_creative_link_caption', 'ad_creative_link_title','ad_creative_link_description', 'aws_ocr_text_img', 'google_asr_text', 'aws_ocr_text_vid'] # nolint
+cols <- c(
+  "ad_id", "page_name", "disclaimer", "ad_creative_body", "ad_creative_link_caption", "ad_creative_link_title",
+  "ad_creative_link_description", "aws_ocr_text_img", "google_asr_text", "aws_ocr_text_vid"
+) # nolint
 # Select only the specified columns
-df = df[cols]
+df <- df[, ..cols]
 
 # Adid to pdid
-adid_to_pageid <- 
+adid_to_pageid <-
   fread(path_adid_to_pageid, colClasses = "character") %>%
   select(ad_id, pd_id)
 
@@ -64,7 +67,8 @@ aliases <- select(aliases, c(id, aliases))
 # Shape to long format
 # Remove empty rows
 # Keep only distinct rows based on pd_id and value
-df <- df %>% filter(wmpid != "") %>%
+df <- df %>%
+  filter(wmpid != "") %>%
   pivot_longer(-c(ad_id, pd_id, wmpid)) %>%
   filter(value != "") %>%
   distinct_at(vars(pd_id, value), .keep_all = T)
@@ -73,6 +77,6 @@ df <- df %>% filter(wmpid != "") %>%
 df <- left_join(df, aliases, by = c("wmpid" = "id"))
 
 # Get rid of ads that have no aliases
-df <- df[is.na(df$aliases) == F,]
+df <- df[is.na(df$aliases) == F, ]
 
 fwrite(df, path_output)
