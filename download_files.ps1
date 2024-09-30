c85581b634a0e71613b213ad79217c8cdccffc18
ajacewicz <ajacewicz@wesleyan.edu>
2024-09-29 21:52:22 -0400

    ps1 file

--- download_files.ps1
+++ download_files.ps1
@@ -17,8 +17,82 @@
 #     }
 # }
 
+
+# SECOND WAY OF DOING IT
 # Download-Files
 
+# # URLs for the files
+# $model_url = "https://figshare.wesleyan.edu/ndownloader/files/46512400"
+# $data_urls = @(
+#     "https://figshare.wesleyan.edu/ndownloader/files/47298157",
+#     "https://figshare.wesleyan.edu/ndownloader/files/47298214"
+# )
+
+# # Filenames for the downloaded files
+# $model_file = "trained_entity_linker.zip"
+# $data_files = @("fb_2022_adid_text.csv.gz", "g2022_adid_01062021_11082022_text.csv.gz")
+
+# # Process input arguments
+# param (
+#     [string]$option
+# )
+
+# # Function to download files
+# function Download-File {
+#     param (
+#         [string]$url,
+#         [string]$output_file
+#     )
+
+#     if (Test-Path $output_file) {
+#         Write-Host "File '$output_file' already exists."
+#     } else {
+#         Invoke-WebRequest -Uri $url -OutFile $output_file
+#         Write-Host "Downloaded '$output_file'."
+#     }
+# }
+
+# # Function to display help
+# function Show-Help {
+#     Write-Host "Usage: .\download_files.ps1 {-model|-data|-all|-help}"
+#     Write-Host
+#     Write-Host "Options:"
+#     Write-Host "  -model    Download the model file only."
+#     Write-Host "  -data     Download the two data files only."
+#     Write-Host "  -all      Download both the model and data files."
+#     Write-Host "  -help     Display this help message."
+# }
+
+
+# switch ($option) {
+#     "-model" {
+#         Write-Host "Downloading model..."
+#         Download-File -url $model_url -output_file $model_file
+#     }
+#     "-data" {
+#         Write-Host "Downloading data files..."
+#         for ($i = 0; $i -lt $data_urls.Count; $i++) {
+#             Download-File -url $data_urls[$i] -output_file $data_files[$i]
+#         }
+#     }
+#     "-all" {
+#         Write-Host "Downloading model and data files..."
+#         Download-File -url $model_url -output_file $model_file
+#         for ($i = 0; $i -lt $data_urls.Count; $i++) {
+#             Download-File -url $data_urls[$i] -output_file $data_files[$i]
+#         }
+#     }
+#     "-help" {
+#         Show-Help
+#     }
+#     Default {
+#         Write-Host "Invalid option. Use -help for usage information."
+#         exit 1
+#     }
+# }
+
+
+# this should work on older windows models 
 # URLs for the files
 $model_url = "https://figshare.wesleyan.edu/ndownloader/files/46512400"
 $data_urls = @(
@@ -29,11 +103,6 @@ $data_urls = @(
 # Filenames for the downloaded files
 $model_file = "trained_entity_linker.zip"
 $data_files = @("fb_2022_adid_text.csv.gz", "g2022_adid_01062021_11082022_text.csv.gz")
-
-# Process input arguments
-param (
-    [string]$option
-)
 
 # Function to download files
 function Download-File {
@@ -55,36 +124,31 @@ function Show-Help {
     Write-Host "Usage: .\download_files.ps1 {-model|-data|-all|-help}"
     Write-Host
     Write-Host "Options:"
-    Write-Host "  -model    Download the model file only."
-    Write-Host "  -data     Download the two data files only."
-    Write-Host "  -all      Download both the model and data files."
-    Write-Host "  -help     Display this help message."
+    Write-Host " -model  Download the model file only."
+    Write-Host " -data  Download the two data files only."
+    Write-Host " -all  Download both the model and data files."
+    Write-Host " -help  Display this help message."
 }
 
 
-switch ($option) {
-    "-model" {
-        Write-Host "Downloading model..."
-        Download-File -url $model_url -output_file $model_file
-    }
-    "-data" {
-        Write-Host "Downloading data files..."
-        for ($i = 0; $i -lt $data_urls.Count; $i++) {
+switch ((Get-Args).Name) {
+     "-model" {
+         Write-Host "Downloading model..."
+         Download-File -url $model_url -output_file $model_file
+     }
+     "-data" {
+         Write-Host "Downloading data files..."
+         for ($i = 0; $i -lt $data_urls.Count; $i++) {
             Download-File -url $data_urls[$i] -output_file $data_files[$i]
-        }
-    }
-    "-all" {
-        Write-Host "Downloading model and data files..."
+         }
+     }
+     "-all" {
+         Write-Host "Downloading model and data files..."
         Download-File -url $model_url -output_file $model_file
-        for ($i = 0; $i -lt $data_urls.Count; $i++) {
+         for ($i = 0; $i -lt $data_urls.Count; $i++) {
             Download-File -url $data_urls[$i] -output_file $data_files[$i]
-        }
-    }
-    "-help" {
+         }
+     }
+     "-help" {
         Show-Help
-    }
-    Default {
-        Write-Host "Invalid option. Use -help for usage information."
-        exit 1
-    }
-}
+     }}
